222914a4c88e377d29bc5686fe1ee4565712b328
OfflineProgrammer <mmalaka@gmail.com>
2019-09-07 16:01:25 -0700

    City Name UT

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -2,10 +2,14 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,7 +22,21 @@ describe('HomePage', () => {
     fixture.detectChanges();
   }));
 
+  afterEach(() => {
+    fixture.destroy();
+    component = null;
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('displays current city name', () => {
+    
+    fixture.detectChanges();
+    de = fixture.debugElement.query(By.css("ion-card-title"));
+    el = de.nativeElement;
+    expect(el.textContent).toContain('City');
+  });
+
 });
